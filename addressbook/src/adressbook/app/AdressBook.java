4366c0433fe0cde722cd00af8cad1da43ee542a8
Grace Hopper <gracie@ilovelinux.ca>
2017-08-25 13:45:51 -0400

    Create addressbook application

---
+++ addressbook/src/adressbook/app/AdressBook.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package adressbook.app;
+import lib.Address;
+
+/**
+ * @author 1633028
+ *
+ */
+public class AdressBook {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+
+
+	}
+
+}
